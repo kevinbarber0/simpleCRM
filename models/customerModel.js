7223ac310fc8fa816acecc2aedf27a24795828ca
Kevin <kevinbarber0@outlook.com>
2023-10-21 18:19:48 -0400

    firstDBroute

---
+++ models/customerModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const customerSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required:[true, "Please enter a product name"]
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+        price: {
+            type: Number,
+            required: true,
+        },
+
+        image: {
+            type: String,
+            required: false,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Customer =mongoose.model('Customer', customerSchema);
+
+module.exports = Customer;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const e = require('express');
 const express = require('express')
 const mongoose = require('mongoose');
+const Customer =require('./models/customerModel')
 const app = express()
 
 
+app.use(express.json())
 
-//routes
 
 app.get('/', (req, res) => {
     res.send('Hello NODE API')
@@ -15,7 +16,21 @@ app.get('/blog', (req, res) => {
     res.send('Hello Kevin')
 })
 
+// app.post('/customer', (req, res) => {
+//     console.log(req.body)
+//     res.send(req.body)
+// })
 
+app.post('/customer', async(req, res) => {
+    try {
+        const customer = await Customer.create(req.body)
+        res.status(200).json(customer);
+
+    } catch(e) {
+        console.log(e.message);
+        res.status(500).json({message: e.message})
+    }
+})
 
 mongoose.set('strictQuery', false)
 mongoose.
