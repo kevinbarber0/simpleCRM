c1a8685793a667f21dff287d53c2dd2de0c82a73
Kevin <kevinbarber0@outlook.com>
2023-10-21 17:11:55 -0400

    moreBoilerSetup

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const e = require('express');
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
+
 
 
 //routes
@@ -8,7 +11,21 @@ app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
+app.get('/blog', (req, res) => {
+    res.send('Hello Kevin')
+})
 
-app.listen(3000, () => {
+
+
+mongoose.set('strictQuery', false)
+mongoose.
+connect('mongodb+srv://admin:0000@cluster0.4bkeksa.mongodb.net/')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, () => {
         console.log('Node API app is running on port 3000')
+});
+
+}).catch((e) => {
+    console.log(e)
 })
